adc39733984f44d491aed9329a401c3d78fab704
PHP CS Fixer <phpcsfixer@example.com>
2022-09-13 09:16:46 +0000

    Fix code style (php-cs-fixer)

--- src/Database/TenantDatabaseManagers/PostgreSQLDatabaseManager.php
+++ src/Database/TenantDatabaseManagers/PostgreSQLDatabaseManager.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy\Database\TenantDatabaseManagers;
 
-use Illuminate\Support\Facades\DB;
 use Stancl\Tenancy\Database\Contracts\TenantWithDatabase;
 
 class PostgreSQLDatabaseManager extends TenantDatabaseManager
